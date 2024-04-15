3480e68e424748df15b25605fac8293374923a03
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 15:33:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ from main_window import My_MainWindow
 
 if __name__ == '__main__':
     my_app = QApplication(sys.argv)
-
-    my_window = QMainWindow()
-    my_centralwidget = QWidget()
-    my_verticallayout = QVBoxLayout()
-
+    my_window = My_MainWindow()
+    
     my_label = QLabel('My little text.')
+    my_label.setStyleSheet('font-size: 50px;')
+    my_window.my_verticallayout.addWidget(my_label)
 
-    my_centralwidget.setLayout(my_verticallayout)
-    my_verticallayout.addWidget(my_label)
-    my_window.setCentralWidget(my_centralwidget)
     my_window.show()
-
     my_app.exec()
