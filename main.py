bb2fdec203496c8a8036dba6c1f94369b0da68ff
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 19:27:13 -0300

    Update main.py
    Code update.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from PySide6.QtWidgets import (QApplication, QLabel)
 from main_window import cls_mainwindow
 
 from PySide6.QtGui import QIcon
+
 from variables import PATH_WINDOW_ICON_PATH
+from display import cls_display
 
 if __name__ == '__main__':
     # Create the application:
@@ -12,17 +14,22 @@ if __name__ == '__main__':
     var_window = cls_mainwindow()
     
     #
-    var_label = QLabel('My little text.')
-    var_label.setStyleSheet('font-size: 50px;')
+    var_label = QLabel('Enter your operation')
+    var_label.setStyleSheet('font-size: 10px;')
 
     var_window.mtd_addwidgettoverticallayout(var_label)
-    var_window.mtd_adjustfixedsize() #A1:
+    # var_window.mtd_adjustfixedsize() #A1:
 
     # Defining icon
     var_icon = QIcon(str(PATH_WINDOW_ICON_PATH))
     var_window.setWindowIcon(var_icon)
     var_app.setWindowIcon(var_icon)
 
+    # Display
+    var_display = cls_display()
+    var_window.mtd_addwidgettoverticallayout(var_display)
+
+    var_window.mtd_adjustfixedsize() #A1:
     # Runs everything
     var_window.show()
     var_app.exec()
