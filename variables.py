80ba7bbf43d5a48e292ac41206c3c06cdd0dd98c
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 19:28:56 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,3 +4,8 @@ from pathlib import Path
 PATH_ROOT_FOLDER = Path(__file__).parent
 PATH_FILES_DIR = PATH_ROOT_FOLDER / 'files'
 PATH_WINDOW_ICON_PATH = PATH_FILES_DIR / 'monkey.jpg'
+
+# Sizing
+var_big_font_size = 40
+var_medium_font_size = 24
+var_small_font_size = 18
