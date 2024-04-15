a7fbf89e01e249d5c0d9a8fd7d03c5d80a5caa1f
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 15:34:10 -0300

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -8,3 +8,11 @@ from PySide6.QtWidgets import (QApplication,
 class My_MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
+
+        self.my_centralwidget = QWidget()
+        self.my_verticallayout = QVBoxLayout()
+
+
+        self.my_centralwidget.setLayout(self.my_verticallayout)
+
+        self.setCentralWidget(self.my_centralwidget)
