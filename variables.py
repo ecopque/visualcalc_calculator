761a8940eac013ac8b71ecf58249256ab99844cd
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 18:45:43 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
-# variables.py
+# variables.py (C)
 from pathlib import Path
 
-ROOT_FOLDER = Path(__file__).parent
-FILES_DIR = ROOT_FOLDER / 'files'
-WINDOW_ICON_PATH = FILES_DIR / 'monkey.jpg'
+PATH_ROOT_FOLDER = Path(__file__).parent
+PATH_FILES_DIR = PATH_ROOT_FOLDER / 'files'
+PATH_WINDOW_ICON_PATH = PATH_FILES_DIR / 'monkey.jpg'
