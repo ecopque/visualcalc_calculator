25e0973e050a38838aaa9622811f871638ef408c
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 14:53:55 -0300

    Add files via upload
    New main for new calculator. ;-)

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+from PySide6.QtWidgets import (QApplication,
+                               QMainWindow,
+                               QWidget,
+                               QVBoxLayout,
+                               QLabel)
+if __name__ == '__main__':
+    my_app = QApplication(sys.argv)
+
+    my_window = QMainWindow()
+    my_centralwidget = QWidget()
+    my_verticallayout = QVBoxLayout()
+
+    my_label = QLabel('My little text.')
+
+    my_centralwidget.setLayout(my_verticallayout)
+    my_verticallayout.addWidget(my_label)
+    my_window.setCentralWidget(my_centralwidget)
+    my_window.show()
+
+    my_app.exec()
