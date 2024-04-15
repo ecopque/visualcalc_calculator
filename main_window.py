e7b203ba935a57894b9e8e3350d02dde6ad06818
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-15 18:45:19 -0300

    Update main_window.py
    Code update.

--- main_window.py
+++ main_window.py
@@ -1,28 +1,28 @@
-# main_window.py
+# main_window.py (B)
 import sys
 from PySide6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout)
 
-class My_MainWindow(QMainWindow):
+class cls_mainwindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
 
         # Set the basic layout
-        self.my_centralwidget = QWidget()
-        self.my_verticallayout = QVBoxLayout()
+        self.var_centralwidget = QWidget()
+        self.var_verticallayout = QVBoxLayout()
 
-        self.my_centralwidget.setLayout(self.my_verticallayout)
-        self.setCentralWidget(self.my_centralwidget)
+        self.var_centralwidget.setLayout(self.var_verticallayout)
+        self.setCentralWidget(self.var_centralwidget)
 
         # Window title
-        self.setWindowTitle('New Calculator - Edson Copque® | linktr.ee/edsoncopque')
+        self.setWindowTitle('New Calculator - Edson Copque® | github/ecopque')
 
     # Last step
-    def my_adjustFixedSize(self):
+    def mtd_adjustfixedsize(self):
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
 
-    def my_addWidgetToVerticalLayout(self, widget: QWidget):
-        self.my_verticallayout.addWidget(widget)
+    def mtd_addwidgettoverticallayout(self, widget: QWidget):
+        self.var_verticallayout.addWidget(widget)
         # self.my_adjustFixedSize() #B1:
 
 #B1: Read A1.
