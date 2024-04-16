180b845da997191ee57dca6135c57ef0f4b6227a
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-16 17:15:54 -0300

    Update display.py
    Code update.

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 #display.py (D)
 from PySide6.QtWidgets import QLineEdit
-from variables import var_big_font_size
+from variables import var_big_font_size, var_text_margin
 from PySide6.QtCore import Qt
 
 class cls_display(QLineEdit):
@@ -12,3 +12,10 @@ class cls_display(QLineEdit):
         self.setStyleSheet(f'font-size: {var_big_font_size}px;')
         self.setMinimumHeight(var_big_font_size * 2)
         self.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        var_margins = []
+        for _ in range(4):
+            var_margins.append(var_text_margin)
+        self.setTextMargins(var_margins[0], var_margins[1], var_margins[2], var_margins[3])
+        # var_margins = [var_text_margin for _ in range(4)]
+        # self.setTextMargins(*var_margins)
