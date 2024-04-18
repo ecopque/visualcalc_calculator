7bb65733ab457b8dce3bb9b8af2e23c9a715a1a5
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-18 10:41:16 -0300

    Update label.py

--- label.py
+++ label.py
@@ -1,7 +1,14 @@
 # label.py (D)
 from PySide6.QtWidgets import QLabel, QWidget
-from typing import Optional
+# from typing import Optional
+from variables import var_small_font_size
+from PySide6.QtCore import Qt
 
 class cls_info(QLabel):
     def __init__(self, text: str, parent: QWidget | None = None) -> None:
                  super().__init__(text, parent)
+                 self.mtd_configstyle()
+    
+    def mtd_configstyle(self):
+            self.setStyleSheet(f'font-size: {var_small_font_size}px;')
+            self.setAlignment(Qt.AlignmentFlag.AlignRight)
