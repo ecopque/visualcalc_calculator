7a382b8113313a045994dee1f8842cdc42dee76b
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-18 10:27:06 -0300

    Update label.py
    Code update.

--- label.py
+++ label.py
@@ -3,5 +3,5 @@ from PySide6.QtWidgets import QLabel, QWidget
 from typing import Optional
 
 class cls_info(QLabel):
-    def __init__(self, text: str, parent: Optional[QWidget] = None) -> None:
+    def __init__(self, text: str, parent: QWidget | None = None) -> None:
                  super().__init__(text, parent)
