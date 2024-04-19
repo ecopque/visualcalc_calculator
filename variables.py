25be2215ee20d82c5746d344fbaa0f791dd0a449
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-19 17:33:42 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -11,3 +11,8 @@ var_medium_font_size = 24
 var_small_font_size = 18
 var_text_margin = 15
 var_minimum_width = 450
+
+# Colors
+var_primary_color = '#1e81b0'
+var_darker1_primary_color = '#16658a'
+var_darker2_primary_color = '#115270'
