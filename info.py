78b30897776626fdecaf87198f502a6f78e89577
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-19 17:35:04 -0300

    Update info.py

--- info.py
+++ info.py
@@ -1,1 +1,34 @@
-# info.py (D)
+# styles.py (F)
+# pip install pyqtdarktheme
+import qdarktheme
+from variables import var_primary_color, var_darker1_primary_color, var_darker2_primary_color
+
+var_qss =  f"""
+    PushButton[cssClass="specialButton"] {{
+        color: #fff;
+        background: {var_primary_color};
+    }}
+    PushButton[cssClass="specialButton"]:hover {{
+        color: #fff;
+        background: {var_darker1_primary_color};
+    }}
+    PushButton[cssClass="specialButton"]:pressed {{
+        color: #fff;
+        background: {var_darker2_primary_color};
+    }}
+"""
+
+def func_setuptheme():
+    qdarktheme.setup_theme(
+        theme='dark',
+        corner_shape='rounded',
+        custom_colors={
+            "[dark]": {
+                "primary": f"{var_primary_color}",
+            },
+            "[light]": {
+                "primary": f"{var_primary_color}",
+            },
+        },
+        additional_qss=var_qss
+    )
