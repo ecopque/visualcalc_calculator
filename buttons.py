b228d70bd1ecc9a2e53525da9c181a780476c75e
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-20 15:52:34 -0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,5 +1,13 @@
 # buttons.py (G)
 from PySide6.QtWidgets import QPushButton
 
+from variables import var_medium_font_size
+
 class cls_button(QPushButton):
-    ...
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mtd_configstyle()
+    
+    def mtd_configstyle(self):
+        self.setStyleSheet(f'font-size:{var_medium_font_size}px;')
+        self.setMinimumSize(50, 30)
