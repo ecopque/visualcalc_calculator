0e2c02dd4e1ddc7a1c99b46522a87ba642fe2e94
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-06 20:00:44 -0300

    Update display.py

--- display.py
+++ display.py
@@ -1,7 +1,8 @@
 #display.py (D)
 from PySide6.QtWidgets import QLineEdit
-from variables import var_big_font_size, var_text_margin, var_minimum_width
+from variables import (var_big_font_size, var_text_margin, var_minimum_width)
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QKeyEvent
 
 from typing import TYPE_CHECKING # Just test.
 if TYPE_CHECKING:
@@ -24,5 +25,13 @@ class cls_display(QLineEdit):
         self.setTextMargins(var_margins[0], var_margins[1], var_margins[2], var_margins[3])
         # var_margins = [var_text_margin for _ in range(4)]
         # self.setTextMargins(*var_margins)
-
         self.setMinimumWidth(var_minimum_width)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        var_key = event.key()
+        var_keysqt = Qt.Key
+        var_isenter = (var_key == var_keysqt.Key_Enter) or (var_key == var_keysqt.Key_Return)
+
+        if var_isenter:
+            print('Enter button.')
+        # return super().keyPressEvent(event)
