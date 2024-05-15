3295adfcbf95f22b58edcc1ae47cd9e2a6910822
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-15 11:02:23 -0300

    Update main_window.py
    Cleaning.

--- main_window.py
+++ main_window.py
@@ -1,34 +1,28 @@
 # main_window.py (B)
-import sys
-from PySide6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QMessageBox)
+from PySide6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QMessageBox) #1:
 
-class cls_mainwindow(QMainWindow):
-    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
-        super().__init__(parent, *args, **kwargs)
+class cls_mainwindow(QMainWindow): #2:
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None: #3:
+        super().__init__(parent, *args, **kwargs) #4:
 
-        # Set the basic layout
-        self.var_centralwidget = QWidget()
-        self.var_verticallayout = QVBoxLayout()
+        self.var_centralwidget = QWidget() #5:
+        self.var_verticallayout = QVBoxLayout() #5:
+        self.var_centralwidget.setLayout(self.var_verticallayout) #6:
+        self.setCentralWidget(self.var_centralwidget) #6:
 
-        self.var_centralwidget.setLayout(self.var_verticallayout)
-        self.setCentralWidget(self.var_centralwidget)
+        self.setWindowTitle('VisualCalc Calculator (beta)') #7:
 
-        # Window title
-        self.setWindowTitle('[beta] Graphic Calculator | eCop PI[A}')
+    def mtd_adjustfixedsize(self): #8:
+        self.adjustSize() #9:
+        self.setFixedSize(self.width(), self.height()) #10:
 
-    # Last step
-    def mtd_adjustfixedsize(self):
-        self.adjustSize()
-        self.setFixedSize(self.width(), self.height())
+    def mtd_addwidgettoverticallayout(self, widget: QWidget): #11:
+        self.var_verticallayout.addWidget(widget) #12:
 
-    def mtd_addwidgettoverticallayout(self, widget: QWidget):
-        self.var_verticallayout.addWidget(widget)
-        # self.my_adjustFixedSize() #B1:
+    def mtd_setupmenu(self): #13:
+        var_menu = self.menuBar() #14:
+        first_menu = var_menu.addMenu('File') #15:
+        first_option =  first_menu.addAction('Open') #16:
 
-    def mtd_setupmenu(self):
-        var_menu = self.menuBar()
-        first_menu = var_menu.addMenu('File')
-        first_option =  first_menu.addAction('Open')
-
-    def mtd_makemsgbox(self):
-        return QMessageBox(self)
+    def mtd_makemsgbox(self): #17:
+        return QMessageBox(self) #18:
