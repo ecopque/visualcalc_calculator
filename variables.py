e73aa974c0011956bab4e8eaaa648e1f2524b518
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-16 19:55:09 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,19 +1,17 @@
 # variables.py (C)
-from pathlib import Path
+from pathlib import Path #1:
 
-PATH_ROOT_FOLDER = Path(__file__).parent
-PATH_FILES_DIR = PATH_ROOT_FOLDER / 'files'
-PATH_WINDOW_ICON_PATH = PATH_FILES_DIR / 'ecop.png'
+PATH_ROOT_FOLDER = Path(__file__).parent #2:
+PATH_FILES_DIR = PATH_ROOT_FOLDER / 'files' #3:
+PATH_WINDOW_ICON_PATH = PATH_FILES_DIR / 'ecop.png' #4:
 
-# Sizing
-var_big_font_size = 40
-var_medium_font_size = 24
-var_small_font_size = 18
-var_text_margin = 15
-var_minimum_width = 450
+var_big_font_size = 40 #5:
+var_medium_font_size = 24 #5:
+var_small_font_size = 18 #5:
+var_text_margin = 15 #5:
+var_minimum_width = 450 #5:
 
-# Colors
-var_primary_color = '#1e81b0'
-var_primary_color_red = '#FF3333'
-var_darker1_primary_color = '#16658a'
-var_darker2_primary_color = '#115270'
+var_primary_color = '#1e81b0' #6:
+var_primary_color_red = '#FF3333' #6:
+var_darker1_primary_color = '#16658a' #6:
+var_darker2_primary_color = '#115270' #6:
