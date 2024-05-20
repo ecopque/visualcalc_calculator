c58eb6b5b2c64e7b53317c4762271ac48c0fd787
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-20 17:12:44 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,22 @@
 # utils.py (H)
-import re
+import re #1:
 
-var_num_or_dot_regex = re.compile(r'^[0-9.]$')
+var_num_or_dot_regex = re.compile(r'^[0-9.]$') #2:
 
-def func_isnumordot(string: str):
+def func_isnumordot(string: str): #3:
     return bool(var_num_or_dot_regex.search(string))
 
-def func_isempty(string: str):
+def func_isempty(string: str): #4:
     return len(string) == 0
 
-def func_converttointorfloat(string: str):
+def func_converttointorfloat(string: str): #5:
     var_number = float(string)
 
     if var_number.is_integer():
         var_number = int(var_number)
     return var_number
 
-def func_isvalidnumber(string: str):
+def func_isvalidnumber(string: str): #6:
     var_valid = False
     try:
         float(string)
